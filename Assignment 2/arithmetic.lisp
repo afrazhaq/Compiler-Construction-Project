7e426265777877a0a57e4f0c888c8d81fea5b607
AfrazH <74556335+afrazhaq@users.noreply.github.com>
2023-06-05 04:00:03 +0500

    Create arithmetic.lisp

---
+++ Assignment 2/arithmetic.lisp
@@ -0,0 +1,7 @@
+
+(format t "(+ 2 4) = ~d ~%" (+ 2 4 )) //Addition
+(format t "(- 2 4) = ~d ~%" (- 2 4 )) //Subtraction
+(format t "(* 2 4) = ~d ~%" (* 2 4 )) //Multiplication
+(format t "(/ 2 4) = ~d ~%" (/2 4 )) //Division
+(format t "(rem 7 4) = ~d ~%" (rem 7 4 )) //Remainder
+(format t "(mod 7 4) = ~d ~%" (mod 7 4 )) //Mod
